a9d86ec82616be94d88e00cdf8152f8600b7b45d
Wendy Tang <wenhuitang0521@gmail.com>
2018-02-13 04:26:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,10 @@
-var playlist = {};
+var playlist = {"Ariana Grande": "Side to Side"};
 
 function updatePlaylist(playlist, artist, song) {
-  
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
 }
